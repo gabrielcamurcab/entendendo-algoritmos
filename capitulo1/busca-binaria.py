0ffaf7054f77dda284a373801e3efacdbbafd285
Gabriel Camurça <projetosdocamurca@gmail.com>
2024-03-18 16:01:15 -0300

    Implements gerar_lista_ordenada function

--- capitulo1/busca-binaria.py
+++ capitulo1/busca-binaria.py
@@ -1,18 +1,28 @@
+def gerar_lista_ordenada(valor_final):
+    contador = 0
+    lista = []
+    for x in range(valor_final):
+        contador = contador + 1
+        lista.append(contador)
+    return lista
+
 def pesquisa_binaria(lista, item):
     baixo = 0
     alto = len(lista) - 1
+    contador = 0
 
     while baixo <= alto:
         meio = (baixo + alto) // 2
         chute = lista[meio]
         if chute == item:
-            return meio
+            return {"iterações": contador, "indice": meio}
         if chute > item:
             alto = meio - 1
         if chute < item:
             baixo = meio + 1
+        contador = contador + 1
     return None
 
-minha_lista = [1, 3, 5, 7, 9]
-print (pesquisa_binaria(minha_lista, 3))
-print (pesquisa_binaria(minha_lista, -1))
+minha_lista = gerar_lista_ordenada(256)
+print (len(minha_lista))
+print (pesquisa_binaria(minha_lista, 256))
