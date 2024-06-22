d10e3d39eb124d5e26c1a80eeb8ae662e6f32128
Gabriel Camurça <projetosdocamurca@gmail.com>
2024-06-22 11:41:46 -0300

    capitulo2 melhorado (ordenacao de musicas com array de tuplas)

--- capitulo2/ordenacao-por-selecao.py
+++ capitulo2/ordenacao-por-selecao.py
@@ -1,27 +1,37 @@
-def buscaMenor (arr):
+import random
+from pprint import pprint
+
+def buscaMaior (arr):
     menor = arr[0]
     menor_indice = 0
     for i in range(1, len(arr)):
-        if arr[i] < menor:
+        if arr[i][1] > menor[1]:
             menor = arr[i]
             menor_indice = i
-        print("iterou buscaMenor")
     return menor_indice
 
 def ordenarPorSelecao (arr):
     novoArr = []
     for i in range(len(arr)):
-        menor = buscaMenor(arr)
+        menor = buscaMaior(arr)
         novoArr.append(arr.pop(menor))
-        print("iterou ordenarPorSelecao")
     return novoArr
 
-print(ordenarPorSelecao([
-    100,
-    65,
-    897,
-    6,
-    99,
-    3,
-    5
-]))
+
+musicas = [
+    ("Bohemian Rhapsody - Queen", random.randint(1, 1000)),
+    ("Hotel California - Eagles", random.randint(1, 1000)),
+    ("Stairway to Heaven - Led Zeppelin", random.randint(1, 1000)),
+    ("Imagine - John Lennon", random.randint(1, 1000)),
+    ("Smells Like Teen Spirit - Nirvana", random.randint(1, 1000)),
+    ("Sweet Child O' Mine - Guns N' Roses", random.randint(1, 1000)),
+    ("Billie Jean - Michael Jackson", random.randint(1, 1000)),
+    ("Hey Jude - The Beatles", random.randint(1, 1000)),
+    ("Like a Rolling Stone - Bob Dylan", random.randint(1, 1000)),
+    ("Rolling in the Deep - Adele", random.randint(1, 1000))
+]
+
+pprint(musicas)
+musicasOrdenadas = ordenarPorSelecao(musicas)
+print("\n")
+pprint(musicasOrdenadas)
